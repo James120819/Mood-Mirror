a699ed0db247eb7ed1e063e8acf52d653f476eaa
James Farrow <James@Coreys-Air.lan>
2025-12-08 19:53:43 -0500

    Fix Render port binding

--- app.py
+++ app.py
@@ -172,4 +172,6 @@ def export_history():
     return send_file(export_filename, as_attachment=True)
     
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
